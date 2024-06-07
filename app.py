389586d05123e99e241db97b2ff9debd264008d7
Rakshit-Sawarn-iitb <rakshitsawarn21@gmail.com>
2024-06-07 16:38:19 +0530

    procfile added

--- app.py
+++ app.py
@@ -70,5 +70,4 @@ def predict():
     return jsonify({'is_genuine': is_genuine})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
